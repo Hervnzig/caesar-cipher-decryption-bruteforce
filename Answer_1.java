01f86aeb4eec5d003ecb71e0c0f187fb5b0c98ed
Herve <hervnzig1@gmail.com>
2020-10-11 23:25:46 +0200

    answer 1 corrected

--- Answer_1.java
+++ Answer_1.java
@@ -4,17 +4,27 @@ public class Answer_1 {
     public static void main(String[] args) {
         String cipher = "jvtwyloluzpisl\"tlzzhnl";
 
-        System.out.println("Our cipher text is: " + cipher);
-        System.out.print("Our plain text is: ");
+        System.out.println("-- Our cipher text is: " + cipher);
 
-//        int k = 7;
         char[] chars = cipher.toCharArray();
         for (int k=3; k<10; k++) {
+            System.out.print("\n\n|| Our plain text is --- using key: " + k + "\n");
             for (char c:chars){
                 c -= k;
-                System.out.println(c);
+//                System.out.print(c);
+                System.out.print(c);
             }
+
         }
+
+//        int k = 7;
+//        char[] chars = cipher.toCharArray();
+//        for (char c:chars){
+//            c -= k;
+//            System.out.print(c);
+//            decryptedMessage += c;
+//        }
+
 
 }
 }
